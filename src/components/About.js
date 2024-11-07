cc28ed66510f8ea6b7c1bfa97c6e7b601593ad4e
ruman23 <mail2ruman.i@gmail.com>
2024-11-07 03:56:36 -0600

    globally change the color and size

--- src/components/About.js
+++ src/components/About.js
@@ -12,11 +12,16 @@ function About() {
     <section id="about">
       <h1>About Me</h1>
       <p>
-        I’m Md Ruman Islam, a dedicated researcher and software engineer with a keen focus on computing and information science. Currently, I’m pursuing my Ph.D. at the University of Nebraska Omaha, where I work on innovative topics like digital twins, deep learning for networking congestion control, and cybersecurity. My academic journey began with a B.Sc. in Computer Science and Engineering from Shahjalal University of Science and Technology in Bangladesh, followed by a rewarding role as a Software Engineer at Samsung R&D Institute.
+      I'm <strong>Md Ruman Islam</strong>, a Ph.D. student in Computing & Information Science at the University of Nebraska Omaha. My research focuses on cybersecurity, cyber-physical systems, IoT, and machine learning. I also have industry experience as a Software Engineer at Samsung R&D Institute.
       </p>
-      <p>
-        My research interests lie in the realms of cybersecurity, cyber-physical systems, IoT, and machine learning. Driven by the challenge of bridging academia and industry, I continuously strive to advance solutions in these fields. I’ve been honored with the GRACA Grant at UNO and the SRBD Icon of the Month award from Samsung, recognizing my dedication and contributions.
-      </p>
+
+      <h2>Research Interests</h2>
+      <ul className="research-interests">
+        <li>Cybersecurity</li>
+        <li>Cyber-Physical Systems</li>
+        <li>Internet of Things (IoT)</li>
+        <li>Machine Learning</li>
+      </ul>
 
       <h2>Contact</h2>
       <div className="contact-info">
@@ -29,14 +34,6 @@ function About() {
           <li><a href={cv} target="_blank" rel="noopener noreferrer"><ImProfile /></a></li>
         </ul>
       </div>
-
-      <h2>Research Interests</h2>
-      <ul className="research-interests">
-        <li>Cybersecurity</li>
-        <li>Cyber-Physical Systems</li>
-        <li>Internet of Things (IoT)</li>
-        <li>Machine Learning</li>
-      </ul>
 
       <h2>Education</h2>
       <div className="education-container">
